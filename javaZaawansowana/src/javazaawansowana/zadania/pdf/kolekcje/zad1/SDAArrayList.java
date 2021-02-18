14240d6208781a6522494070ae8729ce7295fb61
chodekm <chodekm@gmail.com>
2021-02-18 19:09:50 +0100

    Java zaawansowana - PDF - zadania kolekcje - zad 1 szablon

---
+++ javaZaawansowana/src/javazaawansowana/zadania/pdf/kolekcje/zad1/SDAArrayList.java
@@ -0,0 +1,20 @@
+package javazaawansowana.zadania.pdf.kolekcje.zad1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SDAArrayList<T> {
+
+    Object[] elements;
+
+
+    public boolean add(T elementToAdd){
+
+    }
+
+    public T remove(int index){
+
+    }
+
+
+}
